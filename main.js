8932b9029ddc2e11a5d54c92b50338d45028a0c7
Daniel Freitas <dan.freitas.moura@gmail.com>
2022-09-15 17:40:44 -0300

    Added functions to clear input and insert task by pressing enter

--- main.js
+++ main.js
@@ -1,9 +1,30 @@
 function readInput(){
-    return document.getElementById("input-text").value
+    return document.getElementById("input-text").value;
 }
 
+function clearInput(){
+    document.getElementById("input-text").value = "";
+}
 
 function addTask(){
-    document.getElementById("task").innerHTML += 
+    document.getElementById("tasks").innerHTML += 
               "<li>" + readInput() + "</li>";
+    clearInput();
+}
+
+function addTaskEnter(){
+    let input = document.getElementById("input-text");
+    input.addEventListener("keypress", (e) => {
+        if (e.key === "Enter") {
+            addTask();
+        }
+    });
+}
+
+function listIsEmpty(){
+    let tasks = document.getElementById("tasks");
+    console.log(tasks.children)
+    if (tasks.childElementCount === 0){
+        tasks.innerHTML += "<h2>Ainda não há itens inseridos</h2>";
+    }
 }
